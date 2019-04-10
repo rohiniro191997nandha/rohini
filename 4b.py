7ac9d159369ac15c3272b06bb1ec2073b5ebd3ae
rohiniro191997nandha <49307486+rohiniro191997nandha@users.noreply.github.com>
2019-04-10 10:57:22 +0530

    4b.py

---
+++ 4b.py
@@ -0,0 +1,6 @@
+s=input("enter the character") 
+if(s=='a' or s=='b' or s=='c' or s=='d' or s=='e' or s=='f' or s=='g' or s=='h' or s=='i' or s=='j' or s=='k' or s=='l' or s=='m' or s=='n' or s=='o' or s=='p' or s=='q' or s=='r' or s=='s' or s=='t' or s=='u' or  s=='v' or  s=='w' or s=='x' or  s=='y' or  s=='z'):
+  print("alphabet")
+else:
+  print("not an alphabet")
+  
