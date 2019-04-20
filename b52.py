c0077cc713ba955294ed866c862f00ff6673c383
rohiniro191997nandha <49307486+rohiniro191997nandha@users.noreply.github.com>
2019-04-19 23:31:53 -0700

    b52.py

---
+++ b52.py
@@ -0,0 +1,23 @@
+n=int(input("enter the number"))
+if(n==1):
+  print("one")
+elif(n==2):
+  print("two")
+elif(n==3):
+  print("three")
+elif(n==4):
+  print("four")
+elif(n==5):
+  PRINT("five")
+elif(n==6):
+  print("six")
+elif(n==7):
+  print("seven")
+elif(n==8):
+  print("eight")
+elif(n==9):
+  print("nine")
+elif(n==0):
+  print("zero")
+else:
+  print("invalid")
