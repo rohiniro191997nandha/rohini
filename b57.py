e59799542bf1e09b69a8580dcf8a19d601a366d4
rohiniro191997nandha <49307486+rohiniro191997nandha@users.noreply.github.com>
2019-05-06 08:28:49 -0700

    b57.py

---
+++ b57.py
@@ -0,0 +1,11 @@
+x=int(input("enter the count"))
+y=int(input("num")
+l=[]
+count=0
+for i in range(x):
+  num=int(input("numbers")
+  l.append(num)
+for q in l: 
+  if(q==y):
+    count=count+1
+print(count)    
