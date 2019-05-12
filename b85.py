0805629a5f1d2535204b78fa798877220f2baf83
rohiniro191997nandha <49307486+rohiniro191997nandha@users.noreply.github.com>
2019-05-12 06:22:43 -0700

    b85.py

---
+++ b85.py
@@ -0,0 +1,13 @@
+s=input("enter the string")
+len=len(s)
+odd=[]
+even=[]
+for i in range(len):
+  if(i%2==0):
+    even.append(i)
+  else:
+    odd.append(i)
+for j in even:
+  print("even",s[j])
+for r in odd:
+  print("odd",s[r])
