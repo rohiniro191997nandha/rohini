fcbbf26eaa2f5892f70f1ca6ffec3faddf610b96
rohiniro191997nandha <49307486+rohiniro191997nandha@users.noreply.github.com>
2019-05-14 13:41:52 +0530

    b85.py

--- b85.py
+++ b85.py
@@ -2,13 +2,21 @@ s=input("enter the string")
 len=len(s)
 odd=[]
 even=[]
+z=[]
+y=[]
+str=""
+str1=""
 for i in range(len):
   if(i%2==0):
     even.append(i)
   else:
     odd.append(i)
 for j in even:
-  print("even",s[j])
+  z.append(s[j])
+for k in z:
+  str=str+k
 for r in odd:
-  print("odd",s[r])
-print("hai")
+  y.append(s[r])
+for f in y:
+  str1=str1+f
+print(str,str1)
